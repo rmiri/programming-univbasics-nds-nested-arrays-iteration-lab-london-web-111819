6671d35c4afbd350e16630ff86d60b67d0d07a20
Renata Freitas <r.miriuk@gmail.com>
2019-10-15 20:54:06 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -43,7 +43,7 @@ def total_even_pairs(src)
   i = 0
   newElement = 0
   while i < src.length do
-    if src[i][0] && src[i][1] % 2
+    if src[i][0] % 2 && src[i][1] % 2
       newElement = newElement + src[i][0] + src[i][1]
     end
     i += 1
